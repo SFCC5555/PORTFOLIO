c191fa35882970cb37b6803579bf851654c9229f
SFCC5555 <sfcc.5555@gmail.com>
2022-03-17 20:05:55 -0500

    Modificaciones en SECRETNUMBER

--- PORTFOLIODJANGO/SECRETNUMBER/views.py
+++ PORTFOLIODJANGO/SECRETNUMBER/views.py
@@ -1,6 +1,23 @@
-import datetime
+from http.client import HTTPResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     return render(request, "SECRETNUMBER/index.html")
+
+def secretnumber(request):
+
+    x=(request.GET.get('x'))
+    
+    if x == "h":
+        n="Alto mijin"
+    elif x== "l":
+        n="Bajo mijin"
+    else:
+        n="Exacto mijin"
+
+
+    return render(request, "SECRETNUMBER/index.html", {
+        "n":n
+    })
