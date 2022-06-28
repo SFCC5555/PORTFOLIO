a657e00f9150b33bc7f353b9bb3f4a98be5de8c0
SFCC5555 <sfcc.5555@gmail.com>
2022-06-28 16:36:07 -0500

    Avance SPACEGUN

--- PORTFOLIODJANGO/SPACEGUN/static/SPACEGUN/spacegun.js
+++ PORTFOLIODJANGO/SPACEGUN/static/SPACEGUN/spacegun.js
@@ -9,9 +9,17 @@ var r=30; //radio de las "dianas"-rayos
 var a=0; //angulo de trazo
 var z=0; // contador de tiros acertados
 var q=10; // contador de escudo
+var click=0;
 
 //document.addEventListener("keydown",prueba);
                  
+function clean(dibu)
+{
+    clearInterval(dibu)
+    setTimeout(()=>{c.fillStyle="teal";c.fillRect(0,0,1200,600)},200)         
+    setTimeout(()=>{c.clearRect(0,0,1200,600)},400)
+}
+
 function prueba()
 {
     p.innerHTML="SCORE   "+z;
@@ -30,10 +38,53 @@ function masuno(x,y)
     setTimeout(()=>{c.clearRect(x+25,y-42,22,17)},150);
 }
 
+function miss(x,y,evento)
+{
+    if (click>1)
+    
+    {
+        porcounter=0
+        c.fillStyle="magenta";
+        c.font = "lighter 15px Pixel Digivolve";
+        c.textBaseline="middle";
+        c.textAlign="center";
+        c.fillText("miss x0",parseInt(evento.offsetX),parseInt(evento.offsetY));
+        setTimeout(()=>{c.clearRect(parseInt(evento.offsetX)-31,parseInt(evento.offsetY)-2,61,10)},150);
+    }
+
+}
+
+var porcounter=0
+
+function por(x,y,evento)
+{
+    
+    console.log(evento)
+    porcounter++
+    c.fillStyle="yellow";
+    c.font = "lighter 20px Pixel Digivolve";
+    c.textBaseline="bottom";
+    c.textAlign="left";
+    c.fillText("x"+porcounter,x+25,y-25);
+    c.textAlign="center";
+    c.textBaseline="middle";
+    //c.fillText("+",x,y);
+    //c.fillText("+",parseInt(evento.offsetX),parseInt(evento.offsetY));
+    setTimeout(()=>{c.clearRect(x+25,y-42,40,17)},500);
+    
+    if (porcounter%30==0)
+    {
+        clean(dibu);
+    } 
+}
+
+
 s.addEventListener("click",startgame);
 
 function impacto(x,y)
 {
+    clearInterval(dibu)
+    porcounter=0
     c.fillStyle="red";
     //c.font = "lighter italic 115px Pixel Digivolve";
     c.font = "lighter 126px Pixel Digivolve";
@@ -82,8 +133,27 @@ function startgame()
     
     function disparo(evento)
     {
-        if (parseInt(evento.layerX)<=(x+30) & parseInt(evento.layerX)>=(x-30) & parseInt(evento.layerY)<=y+30 & parseInt(evento.layerY)>=y-30)
+        click++;
+        if (parseInt(evento.offsetX)<=(x+20) & parseInt(evento.offsetX)>=(x-14) & parseInt(evento.offsetY)<=(y+16) & parseInt(evento.offsetY)>=(y-16))
         {
+            por(x,y,evento);
+            z=z+porcounter
+            a=0;
+            r=30;
+            x=Math.random()*(1170-30)+30;
+            y=Math.random()*(570-30)+30;
+            p.innerHTML="SCORE   "+z;
+            //console.log(evento);
+
+            if (click%10==0)
+            {
+                dibu=setInterval(dibujar,100000/click);
+            }
+        }
+   
+        else if (parseInt(evento.layerX)<=(x+40) & parseInt(evento.layerX)>=(x-24) & parseInt(evento.layerY)<=(y+40) & parseInt(evento.layerY)>=(y-24))
+        {
+            porcounter=0
             masuno(x,y);
             z++;
             a=0;
@@ -93,11 +163,18 @@ function startgame()
             p.innerHTML="SCORE   "+z;
             //console.log(evento);
             
-            if (z%10==0)
+            if (click%10==0)
             {
-                dibujar()
+                dibu=setInterval(dibujar,100000/click);
             }
         }
+
+        else
+        {
+            miss(x,y,evento)
+        }
+
+
     }
     
     dibujar()   
